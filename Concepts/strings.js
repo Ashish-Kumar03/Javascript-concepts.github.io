6d7f34be03b738e8f6015065c0902b8139bb69a6
Ashish Kumar <prince032000@gmail.com>
2024-09-10 07:41:43 +0000

    Strings and string methods

---
+++ Concepts/strings.js
@@ -0,0 +1,12 @@
+//String Interpolation : `${placeholder}`
+
+const name = new String("Ashish");
+
+console.log(name);
+
+// substring = (start, end);
+//slice = (start- can be negative or starting from reverse of a string, end);
+//trim = removes spaces from starting as well ending.
+//replace = ('element to be replaced', 'replacement element');
+// includes =  checks whether some element are present in string or not  //includes()
+// split = it gives an array of string subelements // split(separator, limit)
